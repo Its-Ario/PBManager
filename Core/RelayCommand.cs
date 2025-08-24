b35dfcbab62effa0436074bc1e78ef01c6ec8f82
Ario Shakeri <itsario900@gmail.com>
2025-08-24 19:15:27 +0330

    Added Basic Template

---
+++ Core/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows.Input;
+
+namespace ChipsManager.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<Object> _execute;
+        private Func<Object, bool> _canExecute;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<Object> execute, Func<Object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
